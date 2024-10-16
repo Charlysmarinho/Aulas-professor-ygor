1ba135e58a124fedef61525886fbaa7e41b5f02e
Charlysmarinho <tiohique_@hotmail.com>
2024-10-16 18:51:41 -0300

    Add files via upload

---
+++ listapoo.py
@@ -0,0 +1,183 @@
+####adição#######
+#d1gto = int(input('insira um número: '))
+#d2gto = int(input('insira um número: '))
+#plu5= d1gto+d2gto
+#print (d1gto, "+", d2gto ,"=", plu5)
+########################################
+############subtracao#############
+#d1gto = int(input("digite um número: "))
+#d2gto = int(input("insira um número: "))
+#su8tr = d1gto-d2gto
+#print (d1gto ,"-", d2gto,"=", su8tr)
+#########################################
+###########multiplicaçao##############
+#d1gto = int(input("digite um número: "))
+#d2gto = int(input("digite um número: "))
+#mult1p = d1gto*d2gto
+#print (d1gto ,"*", d2gto,"=", mult1p)
+############################################
+############divisao#####################
+#d1gto = int(input("digite um número: "))
+#d2gto = int(input("digite um número: "))
+#d1visao = d1gto/d2gto
+#print (d1gto ,"/", d2gto,"=", d1visao)
+###############################################
+###############resto de divisao################
+#d1gto = int(input("digite um número: "))
+#d2gto = int(input("digite um número: "))
+#r3sto = d1gto%d2gto
+#print (d1gto ,"%", d2gto,"=", r3sto)
+###########################################
+################potenciacao###########
+#d1gto = int(input("digite um número: "))
+#exp03nte = int(input("digite um expoente: "))
+#p0tencia = d1gto**exp03nte
+#print (d1gto ,"**", exp03nte,"=", p0tencia)
+################################################
+################calculadora_medias_##################
+#d1gto = int(input("insira nota 1: "))
+#d2gto = int(input("insira nota 2: "))
+#d3gto = int(input("insira nota 3: "))
+#m3dia = (d1gto+d2gto+d3gto)/3
+#print ("a sua média é: " ,m3dia)
+###########################################
+##########conversor_temperatura#############
+#c3lsius = float(input("qual a temperatura em graus celsius?"))
+#f4renheit = c3lsius*1.38+32
+#print (c3lsius, "graus celsius, equivale a", f4renheit,"farenheits")
+#########################################################################
+##################conversor de moeda#########################
+#r3al = float(input("qual o valor em 'real'?"))
+#c0nvers = r3al *5.46
+#print ("R$", r3al,"convertidos é igual a", '$',c0nvers")
+###############################################################
+#################area_de_um_retangulo#####################
+#b4se = float(input("qual o valor da base?"))
+#altur4 = float(input("qual o valor da altura?"))
+#c4lcul = b4se*altur4
+#print ("o valor da area do retangulo cujo a base mede:", b4se, 'e a altura mede:', altur4,'é igual a:', c4lcul)
+################################################################
+#######################perimetro_do_quadrado##################
+#l4do = float(input('insira o valor de um dos lados do quadrado'))
+#p3rimetro = l4do*4
+#print ("o valor da area do quadrado")
+#################################################################
+###########area_do_triangulo########################
+#b4se= float(input("insira o valor da base do triangulo: "))
+#altur4 = float(input("insira a altura do triangulo:"))
+#ar3a = b4se*altur4/2
+#print ("a area do triangulo é: ", ar3a)
+#######################################################
+############area_do_circulo###########################
+#r4io = float(input("insira o valor do raio"))
+#v4lorpi = 3.14
+#ar3a = r4io*v4lorpi**2
+#print  ("o valor da area deste circulo é de: ", ar3a)
+###########################################################
+############conversor de medidas#####################
+#m3tros = float(input("quantos metros? "))
+#c3ntime = m3tros*100
+#print (m3tros,'metros, equivalem a ', c3ntime,"centímetros.")
+##################################################################
+##################horas trabalhadas#################################
+#h0ra = float(input("horas trabalhadas: "))
+#v4lor = float(input("valor acordado:"))
+#c0nta = h0ra*v4lor
+#print ("foram trabalhadas: ", h0ra,"horas", "totalizando: ", c0nta, 'reais')
+#################################################################################
+##################preco com desconto##############################################
+#v4lor_total= float(input("qual o valor da compra? "))
+#d3sconto = int(input("valor do desconto:"))
+#d3sc0nto = (d3sconto*v4lor_total)/100
+#v4lor_descontado = d3sc0nto-v4lor_total
+#print ("\nsua compra no valor de: ", v4lor_total, "\nvalor do desconto: ", d3sconto, "\no valor a pagar é de R$: ", v4lor_descontado)
+######################################################################################
+######################velocidade_media##################################
+#dist4ncia = float(input("qual a distancia (km) em percorrida?"))
+#t3mpo = float(input("qual foi o tempo(hr) gasto?"))
+##print ("sua velocidade Média foi de:",v3locidade)
+################################################################]
+#################conversor anos em dias#########################
+#id4de = int(input("qual dua idade?"))
+#d4ys = id4de*365
+#print ("você esta a:", d4ys,"na terra!")
+###################################################################
+######################conversor_hora_minuto
+#h0ra = int(input("insira a hora: "))
+#h0ra_m1nuto = h0ra*60
+#d1a_s3gundo = h0ra_m1nuto*60
+#print (h0ra , ":hr equivalem a ",h0ra_m1nuto,"minutos, que são:",d1a_s3gundo, "segundos")
+#####################################################################
+#########################calculo_imc##################################
+#p3so = float(input("qual o seu peso atual?"))
+#altur4 = float(input("qual a sua altura?"))
+#imc = p3so/(altur4**2)
+#print (f"seu imc é: {imc:.2f}" )
+#
+#if imc<18.5:
+#    print ("você esta magro demais!vai treinar seu frango!!")
+#elif 18.5<= imc <= 24.9:
+#    print ("seu peso esta normal!")
+#elif 25<= imc <= 29.9:
+#    print (" voce esta com sobrepeso hein!")
+#elif 30<= imc <=39.9:
+#    print ("voce esta com obesidade tipo 1")
+#elif 40<= imc <=100:
+#    print ("voce esta gigaenorme meu chapa")
+###########################################################
+#################numero_absoluto###################
+#def numero_absoluto(n4mero,n4m3ro):
+#    return abs(n4mero-n4m3ro)
+#n4mero = float(input("insira um numero: "))
+#n4m3ro = float(input("inira um numero: "))
+#r3sultado = numero_absoluto(n4mero,n4m3ro)
+#print (f'A diferenca absoluta entre: {n4mero} e {n4m3ro} é {r3sultado}')
+##############################################################################
+####################divisao_inteira########################################
+#n4mero = int(input("insira um numero: "))
+#n4m3ro = int(input("insira um numero: "))
+#d1visao_int = (n4mero//n4m3ro)
+#print (f"a divisao inteira entre o numero {n4mero} e {n4m3ro} é {d1visao_int}.")
+###############################################################
+###############VALOR_ABSOLUTO##################################
+#def numero_absoluto(num3ro):
+#    return abs(num3ro)
+#num3ro = float(input("digite um numero aí cidadão: "))
+#abs0luto = abs(num3ro)
+#print (f"o numero absoluto de {num3ro} é {abs0luto}")
+#################################################################
+##################KM PARA MS ####################################
+#v3locidade = float(input("qual a velocidade?"))
+#c0nversao = v3locidade*(5/18)
+#print (f'a velocidade de {v3locidade} km/h, valem {c0nversao: .2f}m/s')
+#################################################################
+############valor_total_da_compra############################
+#it3m1 = float(input("valor primeiro item: "))
+#it3m2 = float(input("valor segundo item: "))
+#it3m3 = float(input("valor terceiro item: "))
+#t0tal = it3m3 + it3m2 + it3m3
+#print (f"o total da compra é R$:{t0tal: .2f}")
+############################################################
+################DIAS_PARA_SEMANAS###################
+#1as = int(input("informe quantos dias: "))
+
+#s3manas = d1as//7
+#di4s_restantes = d1as %7
+#print (f"{d1as} dias equivalem a {s3manas} semanas e {di4s_restantes} dias.")
+#################################################################
+#############DESCONTO_PROGRESSIVO#################
+#v4lor_total = float(input("valor total da compra: "))
+#d3scont_5 = v4lor_total*5/100
+#d3scont_10 = v4lor_total*10/100
+#if v4lor_total >= 100:
+#    print ("seu valor com desconto: ", v4lor_total - d3scont_5)
+#elif v4lor_total >=500:
+#    print ("seu valor com desconto: ", v4lor_total - d3scont_10)
+#######################################################
+################DIVISAO_2CASAS_DECIMAIS####################
+#num3ro1 = float(input("primeiro número: "))
+#num3ro2 = float(input(" segundo número: "))
+
+#d1visao = num3ro1 / num3ro2
+#print (f"{d1visao: .2f}")
+###########################################################
