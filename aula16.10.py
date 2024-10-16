c91586e92a933daaf6235d63fe4eb868e45a76a6
Charlysmarinho <tiohique_@hotmail.com>
2024-10-16 20:13:43 -0300

    Add files via upload

---
+++ aula16.10.py
@@ -0,0 +1,24 @@
+#VERIFICAR_IDADE##
+#id4de = int(input("insira sua idade: "))
+#if id4de >=18:
+#    print ("você é maior de idade.")
+#else:
+#    print ("você é menor de idade.")
+#######################################
+#IGUAIS_OU_DIFERENTE
+#num3ro1 = float(input("informe um numero: "))
+#num3ro2 = float(input("informe outro numero: "))
+#if num3ro1 == num3ro2:
+#    print ("os dois são iguais")
+#else:
+#    print ("sao diferentes")
+###################################
+#POSITIVO_NEGATIVO_ZERO##
+#num3ro = float(input("escolha um número:"))
+#if num3ro >0:
+#    print (f'numero {num3ro} é positivo.')
+#elif num3ro <0:
+#    print (f'o número {num3ro} é negativo.')
+#else :
+#    print ('o número é zero.')        
+###################################
