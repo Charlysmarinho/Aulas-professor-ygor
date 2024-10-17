2e5c28296bb60c08536389e4e91b404840a4c37f
Charlysmarinho <tiohique_@hotmail.com>
2024-10-16 21:41:43 -0300

    Add files via upload

---
+++ 16.10.py
@@ -0,0 +1,87 @@
+##1.numero_positivo_negativo##
+#num3ro_user = float(input("escolha um número: "))
+#if num3ro_user >0:
+#    print (f'o numero {num3ro_user} é positivo.')
+#elif num3ro_user <0:
+#    print (f'o número { num3ro_user} é negativo. ')
+#else:
+#    print ("o número é zero")
+#--------------------------------------------------
+##2.MAIORIDADE######################
+#id4de = int(input("insira sua idade:"))
+#if id4de >=18:
+#    print (f'idade: {id4de} se lascou, vai preso!')
+#else:
+#    print (f"idade: {id4de} você é menor de idade, vai so apanhar mesmo.")
+#---------------------------------------------------
+##3.PAR_OU_IMPAR#### 
+#num3ro = int(input("digite um número: "))
+#if num3ro%2 == 0:
+#    print (f"{num3ro} é par")
+#else:
+#    print (f"{num3ro}é impar: ")
+##--------------------------------------------------
+##4.NUMERO_MAIOR################################
+#num3_1 = float(input(" digite um número: "))
+#num3_2 = float(input(" digite outro número:"))
+#if num3_1 > num3_2:
+#    print (f"{num3_1} é maior que {num3_2: .2f}.")
+#else:
+#    print (f"{num3_2}é maior que {num3_1: .2f}.")
+##--------------------------------------------
+#5.DESCONTO_NA_COMPRA##########################
+#v4lor_total = float(input("qual o valor total da sua compra?"))
+#d3sconto = v4lor_total*10/100
+#if v4lor_total > 100:
+#    print (f'você recebeu um desconto de 10% em sua compra, seu novo total é de R$:{v4lor_total-d3sconto: .2f}')
+#else: 
+#    print (f'Total da sua compra é de R$:{v4lor_total}')
+##----------------------------------------------
+##6.VERIFICAR_MULTIPLO#################
+#numer0 = float(input("digite um número:"))
+#if numer0 %5 == 0:
+#    print (f'{numer0: .2f} é multiplo de 5.')
+#else:
+#    print(f'{numer0: .2f} não é multiplo de 5.')
+##-----------------------------------------------
+##7.CALCULAR_MÉDIA##################
+#num3r01 = float(input("insira uma nota: "))
+#num3r02 = float(input("insira uma nota: "))
+#num3r03 = float(input("insira uma nota: "))
+#m3dia = (num3r01 + num3r02 + num3r03)/3
+#print (f'a média das sua notas são: {m3dia: .2f}')
+##----------------------------------------------------
+###8.SENHA_CORRETA####################
+#senh4 = input("insira sua senha: ")
+
+#passworld = ("python123_EFG")
+#if senh4 == passworld:
+#    print ("acesso liberado meu jovem.")
+#else:
+#    print ("acesso negado meu parceiro.")
+##------------------------------------------
+##9.ENTRADA_GRATUITA###############
+#idad3 = int(input("qual a idade?"))
+#if idad3<6 or idad3>60:
+#    print (f'parabens você recebeu o nosso passe gratuito ao parque.')
+#else:
+#    print ("você paga o valor integral do ingresso.")
+##----------------------------------------------
+##10.VERIFICAR_NOTA    
+#n0ta = int(input("atribua uma nota ao nosso servico: "))
+#if n0ta >= 0 and n0ta <= 10:
+#    print (f"você atribuiu nota: {n0ta}")
+#else:
+#    print ("erro, atribua uma nota de 0 a 10.")  
+##-------------------------------------------------
+##11.CATEGORIA_DE_IDADE#################
+#id4d3 = int(input('insira sua idade: ')) 
+
+#if id4d3 >= 18:
+#    print (f"você é adulto, sua idade é: {id4d3}.")
+#elif id4d3 <=12:
+#    print (f'você é criança, sua idade é: {id4d3}.')
+#elif id4d3 >=13 and id4d3 <=17:
+#    print (f'você é um chato geracao z, sua idade é: {id4d3}')
+##---------------------------------------------------
+##12.
