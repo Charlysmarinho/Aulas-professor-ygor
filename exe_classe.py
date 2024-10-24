43c1b45546529e57991a634d1f9570eeb89feea8
Charlysmarinho <tiohique_@hotmail.com>
2024-10-23 22:52:37 -0300

    Update exe_classe.py
    corrigido

--- exe_classe.py
+++ exe_classe.py
@@ -4,12 +4,12 @@ class pessoa ():
         self.s0brenome = s0brenome
         self.id4de = id4de
         self.c1dade = c1dade
-n0me = pessoa(input("qual o seu nome? "))
-s0brenome = pessoa(input("qual o seu sobrenome? "))
-id4de = pessoa(int(input("qual sua idade? ")))
-c1dade = pessoa(input("qual sua cidade? "))
-print (f"\nnome:{pessoa.n0me},\nsobrenome:{pessoa.s0brenome}, \nidade:{pessoa.id4de}, cidade:{pessoa.c1dade} ")
-
-
-
+#---entradas-----
+n0me_inp = input("qual o seu nome? ")
+s0brenome_inp = input("qual o seu sobrenome? ")
+id4de_inp = int(input("qual sua idade? "))
+c1dade_inp = input("qual sua cidade? ")
+#----------------------
+p3ssoa = pessoa(n0me_inp, s0brenome_inp, id4de_inp, c1dade_inp)
+print (f"\nnome:{p3ssoa.n0me}\nsobrenome:{p3ssoa.s0brenome} \nidade:{p3ssoa.id4de} \ncidade:{p3ssoa.c1dade} ")
     
